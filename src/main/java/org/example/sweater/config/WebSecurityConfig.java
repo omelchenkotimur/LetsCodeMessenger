5c5e44e12af4a529d850da5941bd273c80ac17cc
Tamerlan <omelchenkotamer@gmail.com>
2020-09-01 12:39:45 +0300

    Post authorship added.

--- src/main/java/org/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/org/example/sweater/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.sweater.config;
 
 
+import org.example.sweater.userservice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,13 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private DataSource dataSource;
+    private UserService userService;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,11 +35,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("SELECT username, password, active FROM usr WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT u.username, ur.roles FROM usr u INNER JOIN user_role ur on u.id = ur.user_id WHERE u.username=?");
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+
+//                .usersByUsernameQuery("SELECT username, password, active FROM usr WHERE username=?")
+//                .authoritiesByUsernameQuery("SELECT u.username, ur.roles FROM usr u INNER JOIN user_role ur on u.id = ur.user_id WHERE u.username=?");
 
     }
 }
--- src/main/java/org/example/sweater/domain/Message.java
+++ src/main/java/org/example/sweater/domain/Message.java
@@ -1,9 +1,6 @@
 package org.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -14,12 +11,21 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER )
+    @JoinColumn (name = "user_id")
+    private org.example.sweater.domain.User author;
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, org.example.sweater.domain.User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
     }
 
     public Integer getId() {
@@ -45,4 +51,8 @@ public class Message {
     public void setTag(String tag) {
         this.tag = tag;
     }
+
+    public User getAuthor() { return author; }
+
+    public void setAuthor(org.example.sweater.domain.User author) { this.author = author; }
 }
